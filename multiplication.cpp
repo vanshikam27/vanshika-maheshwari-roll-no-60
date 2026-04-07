463b7434fe150101354d11b0ca2531f7c279bc39
vanshikam27 <vanshikam488@gmail.com>
2026-04-07 11:42:07 +0530

    Create multiplication.cpp

---
+++ multiplication.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int a,b,mul;
+    cout << "Enter 2 numbers to multiply :" ;
+    cin >> a;
+    cin >> b;
+    mul = a*b;
+    cout << "The multiplication of 2 given numbers is:" << mul;
+    return 0;
+}
