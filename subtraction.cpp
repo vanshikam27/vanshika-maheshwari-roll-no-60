ab1032435bac79dd32f4d1d1d46cea276d1642ba
vanshikam27 <vanshikam488@gmail.com>
2026-04-07 11:43:49 +0530

    Create subtraction.cpp

---
+++ subtraction.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int a,b,sub;
+    cout << "Enter 2 numbers to subtract :" ;
+    cin >> a;
+    cin >> b;
+    sub = a-b;
+    cout << "The subtraction of 2 given numbers is:" << sub;
+    return 0;
+}
