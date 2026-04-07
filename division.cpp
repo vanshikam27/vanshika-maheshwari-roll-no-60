f55d7bbfaca038c78efc06492362babb736d5fef
vanshikam27 <vanshikam488@gmail.com>
2026-04-07 11:39:35 +0530

    Create division.cpp

---
+++ division.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main () {
+    int a,b,div;
+    cout << "Enter 2 numbers to divide :" ;
+    cin >> a;
+    cin >> b;
+    div = a/b;
+    cout << "The division of 2 given numbers is:" << div;
+    return 0;
+}
